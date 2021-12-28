6b0ea8734af004496572fb441b77aa525fd701bd
Saurabh Goyal <saurabh@opoyi.com>
2021-12-28 08:26:27 +0530

    Contact Us Page

---
+++ firstapp/pages/contact.js
@@ -0,0 +1,9 @@
+const contact  = () => {
+    return <>
+       <h1>Saurabh Goyal</h1>
+       <h2>https://sggoyal.blogspot.com/</h2>
+       <h3>sggoyal@gmail.com</h3>
+    </>;
+ };
+ 
+ export default contact;
---
+++ firstapp/pages/hello.js
@@ -0,0 +1,7 @@
+const hello  = () => {
+    return <>
+       <h1>Hello World!</h1>
+    </>;
+ };
+ 
+ export default hello;
