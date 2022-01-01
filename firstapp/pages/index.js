6f128db029563df0b220896a1b1f780dfe925263
Saurabh Goyal <saurabh@opoyi.com>
2022-01-01 14:35:00 +0530

    Use Link component

--- firstapp/pages/index.js
+++ firstapp/pages/index.js
@@ -1,22 +1,24 @@
+import Link from "next/link";
+
 const index  = () => {
    return <>
       <nav>
          <ul className="menu-bar">
-            <li>
-              <a href="/"> Home</a> 
-            </li>
-            <li>
-              <a href="/about"> About </a> 
-            </li>
-            <li>
-              <a href="/contact"> Contact </a> 
-            </li>
-            <li>
-              <a href="/blog"> Blog </a> 
-            </li>
-            <li>
-              <a href="/product"> Product </a> 
-            </li>
+            <Link  href="/">
+              <a> Home</a> 
+            </Link>
+            <Link  href="/about">
+              <a> About </a> 
+            </Link>
+            <Link  href="/contact">
+              <a> Contact </a> 
+            </Link>
+            <Link href="/blog">
+              <a> Blog </a> 
+            </Link>
+            <Link href="/product">
+              <a> Product </a> 
+            </Link>
          </ul>
       </nav>
       <h1>Hello World!</h1>
