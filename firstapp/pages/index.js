ecdefb61d2f5dbbbb26ec2a8b20ce8a5b77b7deb
Saurabh Goyal <saurabh@opoyi.com>
2022-01-02 17:08:27 +0530

    Meta Info

--- firstapp/pages/index.js
+++ firstapp/pages/index.js
@@ -6,6 +6,10 @@ const index  = () => {
    <>
      <Head>
         <title>Home Page</title>
+        <meta name="description" content="Next.Js Tutorials" />
+         <meta name="keywords" content="HTML, CSS, JavaScript and Next.Js" />
+         <meta name="author" content="Saurabh Goyal" />
+         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
      </Head>
      <Header />
    </>
