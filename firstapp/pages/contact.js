4cade7e4922c6ffc7b6a3452988f77a79171d8c5
Saurabh Goyal <saurabh@opoyi.com>
2022-01-02 16:21:29 +0530

    Inline Css in nextJs

--- firstapp/pages/contact.js
+++ firstapp/pages/contact.js
@@ -3,7 +3,7 @@ import Navbar from "../components/Navbar";
 const contact  = () => {
     return <>
        <Navbar />
-       <h1>Saurabh Goyal</h1>
+       <h1 style={{color:"green"}}>Saurabh Goyal</h1>
        <h2>https://sggoyal.blogspot.com/</h2>
        <h3>sggoyal@gmail.com</h3>
     </>;
