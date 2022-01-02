0d1fdc4efa3411995e15e7a4705f80901df6ef13
Saurabh Goyal <saurabh@opoyi.com>
2022-01-02 16:30:45 +0530

    Jsx Style

--- firstapp/pages/contact.js
+++ firstapp/pages/contact.js
@@ -3,9 +3,17 @@ import Navbar from "../components/Navbar";
 const contact  = () => {
     return <>
        <Navbar />
-       <h1 style={{color:"green"}}>Saurabh Goyal</h1>
+       <style jsx>
+          {`
+          h1 {
+             color: blue;
+          }
+          `}
+       </style>
+       {/* <h1 style={{color:"green"}}>Saurabh Goyal</h1> */}
+       <h1>Saurabh Goyal</h1>
        <h2>https://sggoyal.blogspot.com/</h2>
-       <h3>sggoyal@gmail.com</h3>
+       <h3><a href="mailto:sggoyal@gmail.com">sggoyal@gmail.com</a></h3>
     </>;
  };
  
