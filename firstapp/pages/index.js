fb67f91ef351a68e6053a3321e207386a8d35230
Saurabh Goyal <saurabh@opoyi.com>
2022-01-02 16:01:17 +0530

    Play with css

--- firstapp/pages/index.js
+++ firstapp/pages/index.js
@@ -1,10 +1,28 @@
 import Navbar from "../components/Navbar";
+//import styles from "../styles/index.module.css";
 
 const index  = () => {
-   return <>
-      <Navbar/>
-      <h1>Hello World!</h1>
-   </>;
+ return (
+   <>
+     <header className="header">
+        <div className="brand_box">
+         <Navbar/>
+      </div>
+
+      <div className="text_box">
+         <h1 className="heading_primary">
+            <span className="heading_primary_main">
+               Welcome to the world of ReactJS
+            </span>
+            <span className="saurabhtext">Love with JavaScript</span>
+         </h1>
+         <a href="#" className="btn btn_white btn_animation">
+            SaurabhGoyal Technical Blog
+         </a>
+      </div>
+      </header>
+   </>
+   );
 };
 
 export default index;
