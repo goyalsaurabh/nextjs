712c0a8d20d0c1a3fd58099c74b12c32bfd9946e
Saurabh Goyal <saurabh@opoyi.com>
2022-01-02 16:48:51 +0530

    Use External Images

--- firstapp/pages/about.js
+++ firstapp/pages/about.js
@@ -5,7 +5,7 @@ const about  = () => {
     return <>
        <Navbar />
        <h1>This is my about us page!</h1>
-       <Image src="/demo1.jpg" height="300" width="500"></Image>
+       <Image src="https://images.pexels.com/photos/18105/pexels-photo.jpg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" height="300" width="500"></Image>
     </>;
  };
  
