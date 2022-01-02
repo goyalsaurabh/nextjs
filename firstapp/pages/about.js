fa67d698746882cb93f7af3ddbd43a18e5e23307
Saurabh Goyal <saurabh@opoyi.com>
2022-01-02 16:42:01 +0530

    Adding Image into nextJs

--- firstapp/pages/about.js
+++ firstapp/pages/about.js
@@ -1,9 +1,11 @@
 import Navbar from "../components/Navbar";
+import Image from "next/image";
 
 const about  = () => {
     return <>
        <Navbar />
        <h1>This is my about us page!</h1>
+       <Image src="/demo1.jpg" height="300" width="500"></Image>
     </>;
  };
  
