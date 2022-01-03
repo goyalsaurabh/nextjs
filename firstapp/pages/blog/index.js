9ab4654236a3b8695768d3abc7cb009c53a4de96
Saurabh Goyal <saurabh@opoyi.com>
2022-01-03 19:45:36 +0530

    GetStaticProps() for Data Fetching

--- firstapp/pages/blog/index.js
+++ firstapp/pages/blog/index.js
@@ -1,10 +1,24 @@
 import Navbar from "../../components/Navbar";
-
-const blog  = () => {
+export const getStaticProps = async () => {
+   const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+   const data = await res.json();
+   return {
+      props : {
+         data,
+      },
+   };    
+};
+const blog  = ({data}) => {
     return <>
-       <Navbar />
-       <h1>This is my blog page!</h1>
+      <Navbar />
+      {data.map((curElem)=> {
+         return (
+            <div key={curElem.id} className="ssr-styles">
+               <h3>{curElem.id}</h3>
+               <h2>{curElem.title}</h2>
+            </div>
+         );
+      })}
     </>;
  };
- 
  export default blog;
